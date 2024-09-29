d4edca6ef324a86fd8caab036ca1bf9b497a6aa1
SebasCh-07 <sebasch.est@gmail.com>
2024-09-29 16:59:34 -0500

    fin Método multiplicar

--- Clases/src/com/krakedev/Calculadora.java
+++ Clases/src/com/krakedev/Calculadora.java
@@ -1,5 +1,30 @@
 package com.krakedev;
 
 public class Calculadora {
+	public int sumar(int valor1, int valor2) {
+		int resultado;
+		resultado = valor1 + valor2;
+		return resultado;
+	}
 
+	public double restar(double valor1, double valor2) {
+		double resultado;
+		resultado = valor1 - valor2;
+		return resultado;
+	}
+
+	public double multiplicar(double valor1, double valor2) {
+		double producto = valor1 * valor2;
+		return producto;
+	}
+
+	public double dividir(double dividendo, double divisor) {
+		double cociente = dividendo / divisor;
+		return cociente;
+	}
+
+	public double promediar(double valor1, double valor2, double valor3) {
+		double promedio = (valor1 + valor2 + valor3) / 3;
+		return promedio;
+	}
 }
