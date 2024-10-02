fadb841a3974e325552010a0e5d42ce8a9986165
SebasCh-07 <sebasch.est@gmail.com>
2024-10-01 21:17:45 -0500

    Clase TestCuenta parte final

--- Taller1/src/clearminds/cuentas/test/TestCuenta.java
+++ Taller1/src/clearminds/cuentas/test/TestCuenta.java
@@ -22,6 +22,36 @@ public class TestCuenta {
 		cuenta1.imprimir();
 		cuenta2.imprimir();
 		cuenta3.imprimir();
+		System.out.println("-------Valores modificados--------");
+		//------------------------------------------
+		
+		cuenta1.setSaldo(444);
+		cuenta3.setSaldo(567);
+		cuenta2.setTipo("D");
+		
+		System.out.println("-------Valores Iniciales--------");
+		cuenta1.imprimir();
+		cuenta2.imprimir();
+		cuenta3.imprimir();
+		System.out.println("-------Valores modificados--------");
+		
+		//-------------------------------------------
+		
+		Cuenta cuenta4 = new Cuenta("0987");
+		cuenta4.setTipo("A");
+		cuenta4.setSaldo(10);
+		
+		Cuenta cuenta5 = new Cuenta("0557","C",10.00);
+		
+		Cuenta cuenta6 = new Cuenta("06666","A",0.0);
+		
+		System.out.println("-------Valores Iniciales--------");
+		cuenta4.imprimirConMiEstilo();
+		cuenta5.imprimirConMiEstilo();
+		cuenta6.imprimirConMiEstilo();
+		System.out.println("-------Valores modificados--------");
+		
+		
 	}
 
 }
