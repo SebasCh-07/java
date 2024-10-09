8f1972c3a084e50f805cf642fc76efb400657125
SebasCh-07 <sebasch.est@gmail.com>
2024-10-08 22:00:41 -0500

    Ejercicio 1, Reto 16

---
+++ Directorio/src/com/cmc/directorio/entidades/AdminTelefono.java
@@ -0,0 +1,9 @@
+package com.cmc.directorio.entidades;
+
+public class AdminTelefono {
+	public void activarMensajeria(Telefono telf) {
+		if(telf.getOperadora()=="movi") {
+			telf.setTieneWhatsapp(true);
+		}
+	}
+}
---
+++ Directorio/src/com/cmc/directorio/test/TestTelefono2.java
@@ -0,0 +1,20 @@
+package com.cmc.directorio.test;
+
+import com.cmc.directorio.entidades.AdminTelefono;
+import com.cmc.directorio.entidades.Telefono;
+
+public class TestTelefono2 {
+
+	public static void main(String[] args) {
+		Telefono telf = new Telefono("movi","098234234",20);
+		AdminTelefono admin = new AdminTelefono();
+		
+		admin.activarMensajeria(telf);
+		
+		System.out.println("Operadora: " + telf.getOperadora() + ", Numero: " + telf.getNumero() + ", Codigo: "+ telf.getCodigo() + ", Tiene Whats: " + telf.isTieneWhatsapp());
+		
+		
+
+	}
+
+}
